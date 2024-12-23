572ec2afcc272b766bcee47b5af23ad0f79fda5b
Potatolong <Potatolong.aaron@gmail.com>
2024-12-23 09:48:29 -0500

    Delete Controllers_Practice.ps1
    No longer needed.

--- Controllers_Practice.ps1
+++
@@ -1,6 +0,0 @@
-﻿#Trying out controllers 
-
-Get-CustomerRecord | 
-Where-Object { $_.LastActivity -lt (Get-Date).AddDays(-365) } | 
-ConvertTo-HTML | 
-Out-File \\intranet\www\reports\inactive-customers.html
