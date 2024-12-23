b19a75ad7e52e80023928735159ef384d4ff64cc
Potatolong <Potatolong.aaron@gmail.com>
2024-12-23 09:49:19 -0500

    Delete SecurityLog_textfile.ps1
    No longer needed.

--- SecurityLog_textfile.ps1
+++
@@ -1,9 +0,0 @@
-
-
-$date = (Get-Date).adddays(-2)
-(New-Object `
-system.diagnostic.eventlog `
-("security", "server02")).entries |
-where {$_.timewritten -gt $date} | 
-out-file C:\Users\Potat\Documents\Powershell_Script\Security.txt
-
